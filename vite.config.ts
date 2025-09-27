94b4f03c34adad8343126fcbcae708a9c2c72af3
Alejandro-ux359 <aprendisajedeprogramacion0321@gmail.com>
2025-09-27 00:49:14 -0400

    dependencia-compilacion-servermovil

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // Esto permite que otros dispositivos en la misma red vean la app
+    port: 5173, // Puedes dejar el puerto que quieras
+  },
 })
