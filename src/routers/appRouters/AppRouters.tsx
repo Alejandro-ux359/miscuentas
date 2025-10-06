f356c6b457bcbbc03e087a0e95de89d510137908
Alejandro-ux359 <aprendisajedeprogramacion0321@gmail.com>
2025-10-06 16:13:32 -0400

    ajuste a inicio

--- src/routers/appRouters/AppRouters.tsx
+++ src/routers/appRouters/AppRouters.tsx
@@ -1,5 +1,5 @@
 // src/routers/AppRouter.tsx
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "../../pages/home/Home";
 import NotificationPage from "../../pages/notificaciones/Notificaciones";
 import Calculadora from "../../pages/calculadora/Calculadora";
@@ -12,6 +12,7 @@ import Configuracion from "../../pages/configuracion/Configuracion";
 function AppRouter() {
   return (
     <Routes>
+       <Route path="/" element={<Navigate to="/inicio" />} />
       <Route path="/inicio" element={<Home />} />
       <Route path="/notificacion" element={<NotificationPage />} />
       <Route path="/calculadora" element={<Calculadora />} />
