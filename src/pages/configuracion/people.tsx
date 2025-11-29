ff47e7ffdb371f22fe7818d102644d6be703737b
Alejandro-ux359 <aprendisajedeprogramacion0321@gmail.com>
2025-11-29 15:21:56 -0500

    estilo y correciones de configuracion

--- src/pages/configuracion/people.tsx
+++ src/pages/configuracion/people.tsx
@@ -1,101 +1,118 @@
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
-import { Card, CardContent, Typography } from "@mui/material";
+import { Card, CardContent, Divider, Typography } from "@mui/material";
 import { useNavigate } from "react-router-dom";
-import LockResetIcon from '@mui/icons-material/LockReset';
-import CloudDownloadIcon from '@mui/icons-material/CloudDownload';
+import LockResetIcon from "@mui/icons-material/LockReset";
+import CloudDownloadIcon from "@mui/icons-material/CloudDownload";
 
 export default function People() {
   const navigate = useNavigate();
+  const disabled = true;
 
   return (
-
     <>
-    
-  <Card
-  sx={{
-    marginTop:4,
-    width:"90%",
+      <Card
+        sx={{
+          marginTop: 4,
+          width: "90%",
+          mx: "auto",
+          cursor: "pointer",
+          position: "relative",
+          display: "flex",
+          alignItems: "center",
+          border: "2px solid #ccc",
+          borderRadius: "12px",
+          padding: "4px",
+        }}
+        onClick={() => navigate("/perfil")}
+      >
+        <CardContent
+          sx={{
+            display: "flex",
+            gap: 1,
+            alignItems: "center",
+          }}
+        >
+          <AccountCircleIcon fontSize="large" />
+          <Typography variant="h6">Mi perfil</Typography>
+        </CardContent>
+      </Card>
+
+      <Card
+        sx={{
+          marginTop: 4,
+          width: "90%",
+          mx: "auto",
+          cursor: "pointer",
+          position: "relative",
+          display: "flex",
+          alignItems: "center",
+          border: "2px solid #ccc",
+          borderRadius: "12px",
+          padding: "4px",
+        }}
+        onClick={() => navigate("/contrasenya")}
+      >
+        <CardContent
+          sx={{
+            display: "flex",
+            gap: 1,
+            alignItems: "center",
+          }}
+        >
+          <LockResetIcon fontSize="large" />
+          <Typography variant="h6">Cambiar contraseña</Typography>
+        </CardContent>
+      </Card>
+
+      <Card
+        sx={{
+          marginTop: 4,
+          width: "90%",
+          mx: "auto",
+          cursor: disabled ? "not-allowed" : "pointer",
+          opacity: disabled ? 0.7 : 1,
+          pointerEvents: disabled ? "none" : "auto",
+          position: "relative",
+          display: "flex",
+          alignItems: "center",
+          border: "2px solid #ccc",
+          borderRadius: "12px",
+          padding: "4px",
+        }}
+        onClick={!disabled ? () => navigate("/descargarapp") : undefined}
+      >
+        <CardContent
+          sx={{
+            display: "flex",
+            gap: 1,
+            alignItems: "center",
+          }}
+        >
+          <CloudDownloadIcon fontSize="large" />
+          <Typography variant="h6">Descargar APP</Typography>
+        </CardContent>
+      </Card>
+
+      <Divider
+       sx={{
+    width: "80%",
+    marginTop: 17,
     mx: "auto",
-    cursor: "pointer",
-    position: "relative",
-    display: "flex",
-    alignItems: "center",
-    border: "2px solid #ccc",     
-    borderRadius: "12px",         
-    padding: "4px",               
+    borderStyle: "dashed",   
+    borderColor: "#ccc",
   }}
-  onClick={() => navigate("/perfil")}
->
-  <CardContent 
-    sx={{ 
-      display: "flex", 
-      gap: 1, 
-      alignItems: "center", 
-    }}
-  >
-    <AccountCircleIcon fontSize="large" />
-    <Typography variant="h6">Mi perfil</Typography>
-  </CardContent>
-</Card>
+      />
 
-  <Card
-  sx={{
-    marginTop:4,
-    width:"90%",
-    mx: "auto",
-    cursor: "pointer",
-    position: "relative",
-    display: "flex",
-    alignItems: "center",
-    border: "2px solid #ccc",     
-    borderRadius: "12px",         
-    padding: "4px",              
-  }}
-  onClick={() => navigate("/contrasenya")}
->
-  <CardContent 
-    sx={{ 
-      display: "flex", 
-      gap: 1, 
-      alignItems: "center", 
-    }}
-  >
-    <LockResetIcon fontSize="large" />
-    <Typography variant="h6">Cambiar contraseña</Typography>
-  </CardContent>
-</Card>
-
-<Card
-  sx={{
-    marginTop:4,
-    width:"90%",
-    mx: "auto",
-    cursor: "pointer",
-    position: "relative",
-    display: "flex",
-    alignItems: "center",
-    border: "2px solid #ccc",     
-    borderRadius: "12px",         
-    padding: "4px",              
-  }}
-  onClick={() => navigate("/descargarapp")}
->
-  <CardContent 
-    sx={{ 
-      display: "flex", 
-      gap: 1, 
-      alignItems: "center", 
-    }}
-  >
-    <CloudDownloadIcon fontSize="large" />
-    <Typography variant="h6">Descargar APP</Typography>
-  </CardContent>
-</Card>
-
-
-
-</>
-
-
+      <Typography
+        variant="h6"
+        sx={{
+          textAlign: "center",
+          marginTop: 2,
+          opacity: 0.7,
+        }}
+      >
+        Version 1.0.0
+      </Typography>
+    </>
   );
 }
